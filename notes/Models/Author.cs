fbe9461f04ed11426fbfb8a948871af1bbd371cf
gleb8 <gleb8@PC>
2025-09-15 22:59:36 +0300

    Добавьте файлы проекта.

---
+++ notes/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace notes.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Login { get; set; }
+        [Required]
+        public required string PasswordHash { get; set; }
+        public char Rights { get; set; } = 'u'; // u - user, a - admin
+        public List <Note>? Notes { get; set; }
+    }
+}
---
+++ notes/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace notes.Models
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public DateTime Created {get; set;} = DateTime.Now;
+        [Required]
+        public required String Title { get; set; }
+        [Required]
+        public required String Article { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
