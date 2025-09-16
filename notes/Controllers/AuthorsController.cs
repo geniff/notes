1581d2d07c9b154f008d9e05a78c42a24a10bc28
gleb <gorko.g@yandex.ru>
2025-09-16 10:44:45 +0300

    update

--- notes/Controllers/AuthorsController.cs
+++ notes/Controllers/AuthorsController.cs
@@ -1,108 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using notes.Data;
-using notes.Models;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using notes.Data;
+    using notes.Models;
 
-namespace notes.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthorsController : ControllerBase
+    namespace notes.Controllers
     {
-        private readonly notesContext _context;
-
-        public AuthorsController(notesContext context)
+        [Route("api/[controller]")]
+        [ApiController]
+        public class AuthorsController : ControllerBase
         {
-            _context = context;
-        }
+            private readonly ApplicationDbContext _context;
 
-        // GET: api/Authors
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Author>>> GetAuthor()
-        {
-            return await _context.Author.ToListAsync();
-        }
-
-        // GET: api/Authors/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Author>> GetAuthor(int id)
-        {
-            var author = await _context.Author.FindAsync(id);
-
-            if (author == null)
+            public AuthorsController(ApplicationDbContext context)
             {
-                return NotFound();
+                _context = context;
             }
 
-            return author;
-        }
-
-        // PUT: api/Authors/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAuthor(int id, Author author)
-        {
-            if (id != author.Id)
+            // GET: api/Authors
+            [HttpGet]
+            public async Task<ActionResult<IEnumerable<Author>>> GetAuthor()
             {
-                return BadRequest();
+                return await _context.Authors.ToListAsync();
             }
 
-            _context.Entry(author).State = EntityState.Modified;
+            // GET: api/Authors/5
+            [HttpGet("{id}")]
+            public async Task<ActionResult<Author>> GetAuthor(int id)
+            {
+                var author = await _context.Authors.FindAsync(id);
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AuthorExists(id))
+                if (author == null)
                 {
                     return NotFound();
                 }
-                else
-                {
-                    throw;
-                }
+
+                return author;
             }
 
-            return NoContent();
-        }
-
-        // POST: api/Authors
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Author>> PostAuthor(Author author)
-        {
-            _context.Author.Add(author);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
-        }
-
-        // DELETE: api/Authors/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAuthor(int id)
-        {
-            var author = await _context.Author.FindAsync(id);
-            if (author == null)
+            // PUT: api/Authors/5
+            // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+            [HttpPut("{id}")]
+            public async Task<IActionResult> PutAuthor(int id, Author author)
             {
-                return NotFound();
+                if (id != author.Id)
+                {
+                    return BadRequest();
+                }
+
+                _context.Entry(author).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return NoContent();
             }
 
-            _context.Author.Remove(author);
-            await _context.SaveChangesAsync();
+            // POST: api/Authors
+            [HttpPost]
+            public async Task<ActionResult<Author>> PostAuthor(Author author)
+            {
+                _context.Authors.Add(author);
+                await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+                return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+            }
 
-        private bool AuthorExists(int id)
-        {
-            return _context.Author.Any(e => e.Id == id);
+            // DELETE: api/Authors/5
+            [HttpDelete("{id}")]
+            public async Task<IActionResult> DeleteAuthor(int id)
+            {
+                var author = await _context.Authors.FindAsync(id);
+                if (author == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+
+            private bool AuthorExists(int id)
+            {
+                return _context.Authors.Any(e => e.Id == id);
+            }
         }
     }
-}
